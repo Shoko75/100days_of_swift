edadf1520153d6c71ac66bc959ca05fbcfa94e2a
kosho75 <kosho75@gmail.com>
2020-06-02 23:08:02 -0700

    Completed Project16

--- Project16_CapitalCities/Project16_CapitalCities/ViewController.swift
+++ Project16_CapitalCities/Project16_CapitalCities/ViewController.swift
@@ -17,21 +17,22 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 	
 
+		// Annotation data setting
 		let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
 		let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
 		let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
 		let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
 		let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
 		
+		// Adding anotations on mapview
 		mapView.addAnnotations([london, oslo, paris, rome, washington])
 	}
 	
 	
 }
 
+// MARK: MKMapView
 extension ViewController: MKMapViewDelegate {
-	
-	
 	
 	// Annotation Setting
 	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
